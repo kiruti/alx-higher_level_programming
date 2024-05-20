5de92b7fbb0a13f8a4262fff367a9f4a1fd3539b
kiruti <ronald.tunui@gmail.com>
2024-05-20 22:43:58 +0000

    correction of using two prints and grill

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    a = "{0:02d}".format(i)
-    if a != "99":
-        x = a+","
+    if i < 99:
+        print("{0:02d},".format(i), end=" ")
     else:
-        x = "99"
-
-    print(x, end=" ")
+        print("99 \n")
