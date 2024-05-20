13b3a53b6e4ae4f808c4185aac2e97d5813a8a81
kiruti <ronald.tunui@gmail.com>
2024-05-20 20:17:22 +0000

    First solved code

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i < 100:
-        print('{0:02d}'.format(i), end=", ")
+    a = "{0:02d}".format(i)
+    if a != "99":
+         x = a+","
+    else:
+        x = "99"
+
+    print(x , end="")  
+
