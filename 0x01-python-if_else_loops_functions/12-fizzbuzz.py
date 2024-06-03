7aef0b5405b351dc9390605ca877d809eda36ad9
kiruti <ronald.tunui@gmail.com>
2024-06-03 05:08:22 -0700

    Correction on whit spaces

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for x in range(1, 101):
-        if x % 5==0 and x % 3==0:
+        if x % 5 ==0 and x % 3 ==0:
             print("FizzBuzz", end=" ")
         elif x % 5 == 0:
             print("Buzz", end=" ")
