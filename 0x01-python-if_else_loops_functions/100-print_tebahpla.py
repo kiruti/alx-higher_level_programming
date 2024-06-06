e504067a588b3436d05c34ad54b04e48e6a9f902
kiruti <ronald.tunui@gmail.com>
2024-06-06 07:04:31 -0700

    we are blue and red same time

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -4,5 +4,7 @@ for i in range(122, 96, -1):
     if i % 2 == 0:
        a = i
     else: 
-        a = i - 32 
-    print(chr(a), end="")
+        a = i - 32
+
+    b = {}.format(a)
+    print(b, end="")
